efcb437351e07afe458fd58e520683726b90d702
fadilu <fadilurehman143@gmail.com>
2023-08-01 12:41:39 +0530

    WeatherApp using Getx

---
+++ lib/controller.dart
@@ -0,0 +1,89 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'constants.dart' as k;
+
+class HomeController extends GetxController {
+  var isLoaded = false.obs; // Define it as an observable
+  num? temp;
+  num? press;
+  num? hum;
+  num? cover;
+  var cityname = ''.obs; // Define it as an observable
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void onInit() {
+    super.onInit();
+    getCurrentLocation();
+  }
+
+  getCurrentLocation() async {
+    var p = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low,
+      forceAndroidLocationManager: true,
+    );
+
+    // ignore: unnecessary_null_comparison
+    if (p != null) {
+      getCurrentCityWeather(p);
+    } else {
+      print('Data unavailable');
+    }
+  }
+
+  getCityWeather(String cityname) async {
+    var client = http.Client();
+    var uri = '${k.domain}q=$cityname&appid=${k.apiKey}';
+    var url = Uri.parse(uri);
+    var response = await client.get(url);
+    if (response.statusCode == 200) {
+      var data = response.body;
+      var decodeData = json.decode(data);
+      updateUI(decodeData);
+      isLoaded.value = true; // Use .value to update observable
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  getCurrentCityWeather(Position position) async {
+    var client = http.Client();
+    var uri =
+        '${k.domain}lat=${position.latitude}&lon=${position.longitude}&appid=${k.apiKey}';
+    var url = Uri.parse(uri);
+    var response = await client.get(url);
+    if (response.statusCode == 200) {
+      var data = response.body;
+      var decodeData = json.decode(data);
+      updateUI(decodeData);
+      isLoaded.value = true; // Use .value to update observable
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  updateUI(var decodedData) {
+    if (decodedData == null) {
+      temp = 0;
+      press = 0;
+      hum = 0;
+      cover = 0;
+      cityname.value = 'Not available'; // Use .value to update observable
+    } else {
+      temp = decodedData['main']['temp'] - 273;
+      press = decodedData['main']['pressure'];
+      hum = decodedData['main']['humidity'];
+      cover = decodedData['clouds']['all'];
+      cityname.value = decodedData['name']; // Use .value to update observable
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
